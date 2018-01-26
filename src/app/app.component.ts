292e9681ef3bd5a34dc0cc368862d15f59acac1c
abellimac <abellimac@gmail.com>
2018-01-25 22:08:45 -0400

    new commit form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rata puta jajaja';
+	title = 'app';
+	name: string;
+	lastname: string;
+	email: string;
+	website: string;
+	hobbies: string[];
+	stateHobbies: boolean;
+
+	constructor()
+	{
+		this.name = 'Abelito';
+		this.hobbies = ['first', 'second', 'third', 'fourth'];
+		this.email= 'abellimac@gmail.com';
+		this.website = 'http://www.roonbo.com';
+		this.stateHobbies = true;
+	}
+
+	showHobbies()
+	{
+		return this.stateHobbies;
+	}
+
+	toogleHobbies()
+	{
+		this.stateHobbies = !this.stateHobbies
+		return !this.stateHobbies;
+	}
+
+	newHobby(newHobby)
+	{
+		console.log(newHobby.value);
+		this.hobbies.push(newHobby.value);
+		newHobby.value = '';
+		return false;
+	}
 }
